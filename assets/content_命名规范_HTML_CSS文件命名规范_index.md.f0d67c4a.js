ebda63b45cba7cc421ebbe064f65bb6124e11b95
ReturnMars <ReturnMars@users.noreply.github.com>
2023-06-06 04:53:27 +0000

    deploy: a734b5580d0a1bcce955674b081eb60f5df4829b

---
+++ assets/content_命名规范_HTML_CSS文件命名规范_index.md.f0d67c4a.js
@@ -0,0 +1,9 @@
+import{_ as s,o as a,c as n,V as e}from"./chunks/framework.5329f344.js";const S=JSON.parse('{"title":"HTML&CSS 文件命名规范","description":"","frontmatter":{},"headers":[],"relativePath":"content/命名规范/HTML&CSS文件命名规范/index.md","filePath":"content/命名规范/HTML&CSS文件命名规范/index.md","lastUpdated":1686027111000}'),t={name:"content/命名规范/HTML&CSS文件命名规范/index.md"},l=e(`<h1 id="html-css-文件命名规范" tabindex="-1">HTML&amp;CSS 文件命名规范 <a class="header-anchor" href="#html-css-文件命名规范" aria-label="Permalink to &quot;HTML&amp;CSS 文件命名规范&quot;">​</a></h1><p>HTML&amp;CSS 文件命名规范遵循：确保文件命名总是以字母开头而不是数字，且字母一律小写，以下划线连接且不带其他标点符号，如：</p><div class="language-"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki material-theme-palenight"><code><span class="line"><span style="color:#A6ACCD;">&lt;!-- HTML --&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">jdc.html</span></span>
+<span class="line"><span style="color:#A6ACCD;">jdc_list.html</span></span>
+<span class="line"><span style="color:#A6ACCD;">jdc_detail.html</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">&lt;!-- SASS --&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">jdc.scss</span></span>
+<span class="line"><span style="color:#A6ACCD;">jdc_list.scss</span></span>
+<span class="line"><span style="color:#A6ACCD;">jdc_detail.scss</span></span></code></pre></div>`,3),c=[l];function p(o,i,r,d,_,C){return a(),n("div",null,c)}const m=s(t,[["render",p]]);export{S as __pageData,m as default};
